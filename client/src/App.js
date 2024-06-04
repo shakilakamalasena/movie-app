45a47649900e944b645d4210d5a57a6f0caf894a
Shakila Kamalasena <104765058+shakilakamalasena@users.noreply.github.com>
2024-06-04 18:33:10 +0530

    Hero, Trailer, Navbar, Header added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import api from "./api/axiosConfig";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
+import Trailer from "./components/trailer/Trailer";
 
 function App() {
     const [movies, setMovies] = useState();
@@ -11,7 +16,7 @@ function App() {
                 "http://localhost:8080/apis/v1/movies"
             );
 
-            console.log(response.data);
+            // console.log(response.data);
 
             setMovies(response.data);
         } catch (err) {
@@ -23,7 +28,17 @@ function App() {
         getMovies();
     }, []);
 
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<Home movies={movies} />}></Route>
+                    <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
